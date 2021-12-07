9ae96d5186900d5bb4ca8fc60127ec788260c8b2
Karolina Karczewska <karolcia_yahoo94@wp.pl>
2021-12-07 18:25:10 +0100

    Add prompt

--- js/script.js
+++ js/script.js
@@ -13,8 +13,20 @@ if(randomNumber == 2){
 if(randomNumber == 3){
     computerMove = 'nożyce';
   }
-
 printMessage('Mój ruch to: ' + computerMove);
-//uzupełnij go o dwa bloki else if. 
-//Powinny one obsłużyć przypadki wylosowania liczby 2 i 3. W tych sytuacjach zmienna 
-//computerMove ma otrzymać odpowiednią wartość – 'papier' lub 'nożyce'.
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+if(playerInput == '2'){
+    playerMove = 'papier';
+}
+if(playerInput == '3'){
+    playerMove = 'nożyce';
+}
+printMessage('Twój ruch to: ' + playerMove);
